e2580a4f275d86fec5846f3ccb46bc86314171e1
ShaMan123 <shacharnen@gmail.com>
2023-03-06 08:57:46 +0200

    fix stroke calc
    Update ObjectGeometry.ts

--- src/shapes/Object/ObjectGeometry.ts
+++ src/shapes/Object/ObjectGeometry.ts
@@ -412,17 +412,15 @@ export class ObjectGeometry<EventSpec extends ObjectEvents = ObjectEvents>
     origin = new Point(1, 1),
     // @TODO pass t instead
     {
-      // origin = new Point(1, 1),
       applyViewportTransform = this.needsViewportCoords(),
     }: {
-      // origin?: Point;
       applyViewportTransform?: boolean;
     } = {}
   ) {
     const vpt = applyViewportTransform ? this.getViewportTransform() : iMatrix;
     const dimVector = origin
       .multiply(new Point(this.width, this.height))
-      .scalarAdd(!this.strokeUniform ? this.strokeWidth * 2 : 0)
+      .add(origin.scalarMultiply(!this.strokeUniform ? this.strokeWidth : 0))
       .transform(
         applyViewportTransform
           ? multiplyTransformMatrices(
@@ -433,7 +431,7 @@ export class ObjectGeometry<EventSpec extends ObjectEvents = ObjectEvents>
         true
       );
     const strokeUniformVector = getUnitVector(dimVector).scalarMultiply(
-      this.strokeUniform ? this.strokeWidth * 2 : 0
+      this.strokeUniform ? this.strokeWidth : 0
     );
     return dimVector.add(strokeUniformVector);
   }
